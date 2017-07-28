8c534c8b0bff883a0d8f797891e4ddd320ca569a
Nathan Jent <n.jent@edaptive.com>
2017-07-28 12:46:00 -0400

    Fix issue with PYTHON_SCRIPTS variable.

--- user_path.bat
+++ user_path.bat
@@ -4,10 +4,10 @@
 set msbuildpath="%ProgramFiles(x86)%\MSBuild\14.0\bin\amd64\"
 
 :: Editor command used to open unassociated filetypes
-setx EDITOR %%VIM%%\gvim
+setx EDITOR %%VIM%%\vim
 
 :: Scripts to include on path
-setx PYTHON_SCRIPTS %%PYTHON3%%\Scripts
+set PYTHON_SCRIPTS="%%PYTHON3%%\Scripts"
 
 setx XDG_CONFIG_HOME %%LOCALAPPDATA%%
 
@@ -15,4 +15,4 @@ setx RUST_SRC_PATH %%USERPROFILE%%.rustup\toolchains\nightly-x86_64-pc-windows-m
 
 :: My User path environment variables
 :: This will store a dynamic reference to these registry values the values should be set manually depending on locations
-setx PATH %%BIN%%;%%CMAKE%%;%%CARGO%%;%%EMSDK%%;%%ECLIPSE%%;%%NODE%%;%%PYTHON3%%;%%PYTHON2%%;%%PYTHON_SCRIPTS%%;%%CLANG%%;%%VIM%%;%%GNUPLOT%%;%%MINGW_BIN%%;%%MINGW_USR_BIN%%;%msbuildpath%
+setx PATH %%BIN%%;%%CMAKE%%;%%CARGO%%;%%EMSDK%%;%%ECLIPSE%%;%%NODE%%;%%PYTHON3%%;%%PYTHON2%%;%PYTHON_SCRIPTS%;%%CLANG%%;%%VIM%%;%%GNUPLOT%%;%%MINGW_BIN%%;%%MINGW_USR_BIN%%;%msbuildpath%
