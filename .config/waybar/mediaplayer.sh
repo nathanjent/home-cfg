5e072a5375019b93d7aecd4e6ff0fb6b5406498c
Nathan Jent <nathanjent@gmail.com>
2024-04-03 23:39:44 -0400

    Update waybar style for media control

--- .config/waybar/mediaplayer.sh
+++ .config/waybar/mediaplayer.sh
@@ -6,9 +6,9 @@ metadata=$(playerctl metadata --format '{{ artist }} - {{ title }}')
 # percentage sets pause/play icon
 
 if [ "$status" = "Playing" ]; then
-    echo "{ \"percentage\": 100, \"text\": \"$metadata\", \"tooltip\": \"$status\", \"class\": \"playerctl-${status,,}\"  }"
+    echo "{ \"percentage\": 100, \"text\": \"$metadata\", \"tooltip\": \"$status\", \"class\": \"${status,,}\"  }"
 elif [ "$status" = "Paused" ]; then
-    echo "{ \"percentage\": 0, \"text\": \"$metadata\", \"tooltip\": \"$status\", \"class\": \"playerctl-${status,,}\" }"
+    echo "{ \"percentage\": 0, \"text\": \"$metadata\", \"tooltip\": \"$status\", \"class\": \"${status,,}\" }"
 fi
 
 
