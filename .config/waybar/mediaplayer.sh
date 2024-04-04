9e636fdb83600579070001e018e068a34c25a352
Nathan Jent <nathanjent@gmail.com>
2024-04-03 23:18:38 -0400

    Add media control to waybar

---
+++ .config/waybar/mediaplayer.sh
@@ -0,0 +1,14 @@
+#!/bin/bash
+
+status=$(playerctl status)
+metadata=$(playerctl metadata --format '{{ artist }} - {{ title }}')
+
+# percentage sets pause/play icon
+
+if [ "$status" = "Playing" ]; then
+    echo "{ \"percentage\": 100, \"text\": \"$metadata\", \"tooltip\": \"$status\", \"class\": \"playerctl-${status,,}\"  }"
+elif [ "$status" = "Paused" ]; then
+    echo "{ \"percentage\": 0, \"text\": \"$metadata\", \"tooltip\": \"$status\", \"class\": \"playerctl-${status,,}\" }"
+fi
+
+
